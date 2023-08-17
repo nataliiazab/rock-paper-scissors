8b00210020b58a9a56029f4574d56ca881caad5e
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2023-08-17 23:42:27 +0100

    improve the name feature

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 /* --- VARIABLES --- */
-const choices = ['✊', '🤚', '✌️'];
-const player1 = document.getElementById('player-1');
-const player2 = document.getElementById('player-2');
-const resultArea = document.getElementById('result-area');
-const playBtn = document.getElementById('play-btn');
+const choices = ["✊", "🤚", "✌️"];
+const player1 = document.getElementById("player-1");
+const player2 = document.getElementById("player-2");
+const player1NameDisplay = document.getElementById("player1-name-display");
+const player2NameDisplay = document.getElementById("player2-name-display");
+const resultArea = document.getElementById("result-area");
+const playBtn = document.getElementById("play-btn");
+const addPlayer1Btn = document.getElementById("add-player1");
+const addPlayer2Btn = document.getElementById("add-player2");
 
 /* --- FUNCTIONS --- */
 const generateChoice = () => {
@@ -11,17 +15,23 @@ const generateChoice = () => {
   return choices[r];
 };
 
-const insertHTML = (choice1, choice2, result) => {
-  player1.innerHTML = choice1;
-  player2.innerHTML = choice2;
-  resultArea.innerHTML = result;
-  resultArea.style.color = 'black';
+const insertHTML = (emoji, playerName, playerElement) => {
+  playerElement.querySelector(".emoji").textContent = emoji;
+  playerElement.querySelector(".name-display").textContent = playerName;
 };
 
 const decideWinner = (a, b, player1Name, player2Name) => {
-  if ((a === '✊' && b === '✊') || (a === '🤚' && b === '🤚') || (a === '✌️' && b === '✌️')) {
+  if (
+    (a === "✊" && b === "✊") ||
+    (a === "🤚" && b === "🤚") ||
+    (a === "✌️" && b === "✌️")
+  ) {
     return "It's a draw!";
-  } else if ((a === '✊' && b === '✌️') || (a === '🤚' && b === '✊') || (a === '✌️' && b === '🤚')) {
+  } else if (
+    (a === "✊" && b === "✌️") ||
+    (a === "🤚" && b === "✊") ||
+    (a === "✌️" && b === "🤚")
+  ) {
     return `${player1Name} won!`;
   } else {
     return `${player2Name} won!`;
@@ -29,20 +39,34 @@ const decideWinner = (a, b, player1Name, player2Name) => {
 };
 
 const play = () => {
-  const player1Name = document.getElementById('player1-name').value;
-  const player2Name = document.getElementById('player2-name').value;
+  const player1Name = document.getElementById("player1-name").value;
+  const player2Name = document.getElementById("player2-name").value;
 
-  if (player1Name === '' || player2Name === '') {
+  if (player1Name === "" || player2Name === "") {
     resultArea.innerHTML = "Please enter both players' names.";
-    resultArea.style.color = 'red';
+    resultArea.style.color = "red";
   } else {
     let choice1 = generateChoice();
     let choice2 = generateChoice();
     let result = decideWinner(choice1, choice2, player1Name, player2Name);
 
-    insertHTML(choice1, choice2, result);
+    insertHTML(choice1, player1Name, player1);
+    insertHTML(choice2, player2Name, player2);
+
+    resultArea.innerHTML = result;
+    resultArea.style.color = "black";
   }
 };
 
 /* --- EVENT LISTENERS --- */
-playBtn.addEventListener('click', play);
+playBtn.addEventListener("click", play);
+
+addPlayer1Btn.addEventListener("click", () => {
+  const playerNameInput = document.getElementById("player1-name");
+  player1NameDisplay.textContent = playerNameInput.value;
+});
+
+addPlayer2Btn.addEventListener("click", () => {
+  const playerNameInput = document.getElementById("player2-name");
+  player2NameDisplay.textContent = playerNameInput.value;
+});
